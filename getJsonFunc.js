057d4df56bd54d8f28614051918d634bf84ec01f
zhangyuanze <735043793@qq.com>
2017-08-23 17:43:15 +0800

    getJsonFunc.js

---
+++ getJsonFunc.js
@@ -0,0 +1,19 @@
+// --file.js--
+function getJSON(url, callback) {
+  let xhr = new XMLHttpRequest();
+  xhr.onload = function () { 
+    callback(this.responseText) 
+  };
+  xhr.open('GET', url, true);
+  xhr.send();
+}
+
+export function getUsefulContents(url, callback) {
+  getJSON(url, data => callback(JSON.parse(data)));
+}
+
+// --main.js--
+import { getUsefulContents } from 'file';
+getUsefulContents('http://www.example.com', data => {
+  doSomethingUseful(data);
+});
