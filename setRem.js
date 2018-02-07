9a79de591933c3323c776df53bbd9295d4e90df5
zhangyuanze <735043793@qq.com>
2018-02-07 15:46:33 +0800

    Update setRem.js

--- setRem.js
+++ setRem.js
@@ -1,11 +1,9 @@
 (function(psdw){
-  var dpr=0 , rem=0 , scale=0;
+  var dpr=0 , rem=0;
   var htmlDOM=document.documentElement;
   dpr=window.devicePixelRatio;
   var currentWidth=htmlDOM.clientWidth;
-  scale=currentWidth/psdw;
-  rem=psdw/10;
-  rem=rem*scale;
+      rem=currentWidth/psdw*100;
   htmlDOM.style.fontSize=rem+'px';
   htmlDOM.setAttribute('data-dpr',dpr)
 })(750)
