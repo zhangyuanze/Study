33102c1c14b90fcb1690412a6438c0b8db50e7c6
zhangyuanze <735043793@qq.com>
2018-03-13 09:08:03 +0800

    utils代码收集

--- getMonDayNum.js
+++ getMonDayNum.js
@@ -1,5 +1,23 @@
-		function getDaysInMonth(year, month) {
+
+//获取某个月天数
+function getDaysInMonth(year, month) {
 			month = parseInt(month, 10);
 			var temp = new Date(year, month, 0);
 			return temp.getDate();
 		}
+
+
+
+//debounce
+function debounce(func, delay) {
+  let timer
+
+  return function (...args) {
+    if (timer) {
+      clearTimeout(timer)
+    }
+    timer = setTimeout(() => {
+      func.apply(this, args)
+    }, delay)
+  }
+}
