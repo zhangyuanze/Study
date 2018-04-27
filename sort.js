8aa58555842536b007d1cb48a227b7e1e837ca5d
zhangyuanze <735043793@qq.com>
2018-04-27 10:41:45 +0800

    排序

--- sort.js
+++ sort.js
@@ -10,3 +10,16 @@ const mergeSort = function loop (arr) {
   return [...loop(left), base, ...loop(right)]
 }
 mergeSort(arr)
+
+
+
+
+function sortgroupcity(originalObj){
+  let sortobj = {};
+  for (let i = 65; i <= 90; i++) {
+      if (originalObj[String.fromCharCode(i)]) {
+          sortobj[String.fromCharCode(i)] = originalObj[String.fromCharCode(i)];
+      }
+  }
+  return sortobj
+}
